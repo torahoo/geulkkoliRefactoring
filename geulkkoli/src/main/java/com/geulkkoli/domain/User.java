95637c11e5aac481fa87e075ba76051dda0b48e7
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2023-03-27 16:56:39 +0900

    #2 feat: 유저 테스트 코드 작성.

--- geulkkoli/src/main/java/com/geulkkoli/domain/User.java
+++ geulkkoli/src/main/java/com/geulkkoli/domain/User.java
@@ -1,6 +1,14 @@
 package com.geulkkoli.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
 public class User {
     private Long userId;
-    private String usrName;
+    private String userName;
+
+
 }
