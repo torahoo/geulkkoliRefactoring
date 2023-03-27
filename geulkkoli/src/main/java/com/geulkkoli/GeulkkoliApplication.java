bc76d8202cad9b5a0ed8c14116fcf1884f65aec9
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2023-03-27 11:27:53 +0900

    feat: 프로젝트 초기 세팅.

---
+++ geulkkoli/src/main/java/com/geulkkoli/GeulkkoliApplication.java
@@ -0,0 +1,13 @@
+package com.geulkkoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeulkkoliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeulkkoliApplication.class, args);
+    }
+
+}
