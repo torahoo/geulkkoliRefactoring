7be15577a9f37b17f91bc0d41e6e178e806a7413
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2023-03-28 11:18:28 +0900

    #2 fix: post 필드명 postNo로 일관되게 수정.

--- geulkkoli/src/main/java/com/geulkkoli/domain/Post.java
+++ geulkkoli/src/main/java/com/geulkkoli/domain/Post.java
@@ -18,7 +18,7 @@ public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long borderNo;
+    private Long postNo;
     private String title;
     private String postBody;
     private String nickName;
