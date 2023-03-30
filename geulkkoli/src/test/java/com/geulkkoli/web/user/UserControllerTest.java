e542e694a242b268ed5006576a656a506db650c0
takoeats <cleverfish1548@gmail.com>
2023-03-30 16:03:28 +0900

    #6 testFix : 세션 유지 확인 테스트 코드 변경

--- geulkkoli/src/test/java/com/geulkkoli/web/user/UserControllerTest.java
+++ geulkkoli/src/test/java/com/geulkkoli/web/user/UserControllerTest.java
@@ -6,15 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-
-import javax.annotation.PostConstruct;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -25,26 +22,12 @@ class UserControllerTest {
     @Autowired
     UserRepository userRepository;
 
-    MockHttpSession mockHttpSession;
-
-    @PostConstruct // 1
-    public void setUp() throws Exception {
-        mockHttpSession = new MockHttpSession();
-
-        mockHttpSession.setAttribute("loginUser", "테스트");
-    }
-
-
     @Test
-    void userLogin() throws Exception{
+    void userLogin() throws Exception {
         //given
         MultiValueMap<String, String> query_param = new LinkedMultiValueMap<>();
         query_param.add("userId", "kkk");
         query_param.add("password", "1234");
-
-        LoginForm loginForm = new LoginForm();
-        loginForm.setUserId("kkk");
-        loginForm.setPassword("1234");
 
         //when
         mockMvc.perform(MockMvcRequestBuilders.post("/login")
@@ -56,8 +39,9 @@ class UserControllerTest {
     @DisplayName("세션이 상태유지를 하는 지 테스트 한다.")
     void isSessionStateful() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/login")
-                .session(mockHttpSession))
+                        .param("userId", "kkk")
+                        .param("password", "1234"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andDo(MockMvcResultHandlers.log());
+                .andDo(MockMvcResultHandlers.print());
     }
 }
