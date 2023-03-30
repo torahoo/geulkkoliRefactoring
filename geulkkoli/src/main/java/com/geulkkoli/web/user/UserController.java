494da1e7171623be3356cf49f044f3f23fdf794c
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2023-03-30 12:19:11 +0900

    #4 feat: user controller에서 로그인 상태 유지를 위한 세션 추가, 입력 값 결과를 받기 위해 bindingResult 매개변수로 추가, 입력 값 오류 또는 존재하지 않는 사용자 일 경우 예외 문구를 반환하게 만듬.

--- geulkkoli/src/main/java/com/geulkkoli/web/user/UserController.java
+++ geulkkoli/src/main/java/com/geulkkoli/web/user/UserController.java
@@ -1,17 +1,29 @@
 package com.geulkkoli.web.user;
 
+import com.geulkkoli.domain.user.User;
+import com.geulkkoli.domain.user.service.LoginService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @Controller
 @Slf4j
-public class UserController {
 
-    private LoginForm form;
+public class UserController {
+    public static final String LOGIN_FORM = "user/loginForm";
+    private final LoginService loginService;
+
+    public UserController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
 
     @GetMapping("/login")
     public String loginForm() {
@@ -19,8 +31,19 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public String userLogin(@ModelAttribute LoginForm form) {
+    public String userLogin(@ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
         log.info("userId {} , password {}", form.getUserId(), form.getPassword());
-        return "user/loginForm";
+        if (bindingResult.hasErrors()) {
+            return LOGIN_FORM;
+        }
+        Optional<User> loginUser = loginService.login(form.getUserId(), form.getPassword());
+
+        if (loginUser.isPresent()) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return LOGIN_FORM;
+        }
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.LOGIN_USER, loginUser);
+        return LOGIN_FORM;
     }
 }
