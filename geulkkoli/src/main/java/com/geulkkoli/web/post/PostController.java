88086f7a013b3f9d51cb6adc15644d83ec9466f7
torahoo <legokbs@gmail.com>
2023-04-04 12:31:11 +0900

    feature/#12-postDeleteForm : 1. board로 되어있던 모든 부분을 post로 바꾸었습니다.

--- geulkkoli/src/main/java/com/geulkkoli/web/post/PostController.java
+++ geulkkoli/src/main/java/com/geulkkoli/web/post/PostController.java
@@ -11,71 +11,71 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
-@RequestMapping("/board")
+@RequestMapping("/post")
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;
 
     @GetMapping("/list")
-    public String boardList(Model model) {
+    public String postList(Model model) {
         model.addAttribute("list", postService.findAll());
-        return "/board/boardList";
+        return "/post/postList";
     }
 
     @GetMapping("/add")
-    public String boardAddForm(Model model) {
+    public String postAddForm(Model model) {
         model.addAttribute("post", new Post());
-        return "/board/boardAddForm";
+        return "/post/postAddForm";
     }
 
     @PostMapping("/add")
-    public String boardAdd(@ModelAttribute Post post, RedirectAttributes redirectAttributes) {
+    public String postAdd(@ModelAttribute Post post, RedirectAttributes redirectAttributes) {
         log.info("title={}", post.getTitle());
         postService.savePost(post);
         redirectAttributes.addAttribute("addStatus", true);
         redirectAttributes.addAttribute("postNo", post.getPostNo());
-        return "redirect:/board/read/{postNo}";
+        return "redirect:/post/read/{postNo}";
     }
 
     @GetMapping("/read/{postNo}")
-    public String boardRead(Model model, @PathVariable Long postNo) {
+    public String postRead(Model model, @PathVariable Long postNo) {
         log.info("postNo={}", postNo);
         Post findPost = postService.findById(postNo);
         model.addAttribute("post", findPost);
-        return "/board/boardPage";
+        return "/post/postPage";
     }
 
     @GetMapping("/update/{postNo}")
-    public String boardUpdateForm(Model model, @PathVariable Long postNo) {
+    public String postUpdateForm(Model model, @PathVariable Long postNo) {
         log.info("updateParam={}, postNo={}", model.getAttribute("post"), postNo);
         Post findPost = postService.findById(postNo);
         log.info("findPost={}", findPost.getPostBody());
         model.addAttribute("post", findPost);
-        return "/board/boardEditForm";
+        return "/post/postEditForm";
     }
 
     @PostMapping("/update/{postNo}")
-    public String boardUpdate(@ModelAttribute Post updateParam, @PathVariable Long postNo, RedirectAttributes redirectAttributes) {
+    public String postUpdate(@ModelAttribute Post updateParam, @PathVariable Long postNo, RedirectAttributes redirectAttributes) {
         log.info("updateParam={}, postId={}", updateParam.getPostBody(), postNo);
         postService.updatePost(postNo, updateParam);
         redirectAttributes.addAttribute("updateStatus", true);
 
-        return "redirect:/board/read/{postNo}";
+        return "redirect:/post/read/{postNo}";
     }
 
     @DeleteMapping("/delete/{postNo}")
-    public String boardDelete(@PathVariable Long postNo) {
+    public String postDelete(@PathVariable Long postNo) {
         log.info("deletePostNo={}", postNo);
         postService.deletePost(postNo);
 
-        return "redirect:/board/list";
+        return "redirect:/post/list";
     }
 
     @PostMapping("/test")
     public String testBlanc(@ModelAttribute Post post) {
         log.info("testBlanc={}", post.getTitle());
 //        postService.savePost(post);
-        return "redirect:/board/list";
+        return "redirect:/post/list";
     }
 }
