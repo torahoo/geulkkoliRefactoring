e46f1b5497ca600d051b00e0c6de3b3fe3072391
신채안 <plaz7@naver.com>
2023-04-06 10:22:25 +0900

    #19 feat: 회원 정보 폼 불러오는 기능 및 HomeController 생성

---
+++ geulkkoli/src/main/java/com/geulkkoli/web/home/HomeController.java
@@ -0,0 +1,17 @@
+package com.geulkkoli.web.home;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class HomeController {
+
+    @GetMapping("/index")
+    public String home() {
+        return "/index";
+    }
+}
