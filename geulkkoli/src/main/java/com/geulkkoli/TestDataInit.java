0ba1d45ce99e55b37d32ccc134893fb86714ad83
torahoo <legokbs@gmail.com>
2023-04-13 14:46:35 +0900

    feature/#36-PostPageAuthorId : 1. ImplPostRepositoryTest -> PostRepositoryTest로 변경 2. PostRepositoryTest를 TestDataInit과 상관없이 별개로 동작하게 끔 설정 -> @ActiveProfiles("test") 추가 후 TestDataInit 클래스 자체에 @Profiles("!test") 어노테이션 추가 3. PostRepositoryTest에 update 메서드 수정

--- geulkkoli/src/main/java/com/geulkkoli/TestDataInit.java
+++ geulkkoli/src/main/java/com/geulkkoli/TestDataInit.java
@@ -8,12 +8,14 @@ import com.geulkkoli.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
+@Profile("!test")
 public class TestDataInit {
 
     private final PostRepository postRepository;
---
+++ geulkkoli/src/test/java/com/geulkkoli/domain/post/PostRepositoryTest.java
@@ -0,0 +1,120 @@
+package com.geulkkoli.domain.post;
+
+import com.geulkkoli.domain.user.ImplUserRepository;
+import com.geulkkoli.domain.user.User;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+class PostRepositoryTest {
+
+    @Autowired
+    private ImplPostRepository implPostRepository;
+    @Autowired
+    private ImplUserRepository impluserRepository;
+
+
+    @AfterEach
+    void afterEach () {
+        implPostRepository.clear();
+    }
+
+    @BeforeEach
+    void beforeEach () {
+        implPostRepository.save(Post.builder()
+                .authorId(1L)
+                .nickName("바나나")
+                .postBody("나는 멋지고 섹시한 개발자")//채&훈
+                .title("여러분").build()
+        );
+        implPostRepository.save(Post.builder()
+                .authorId(2L)
+                .title("testTitle01")
+                .postBody("test postbody 01")
+                .nickName("점심뭐먹지").build()
+        );
+        implPostRepository.save(Post.builder()
+                .authorId(2L)
+                .title("testTitle02")
+                .postBody("test postbody 02")
+                .nickName("점심뭐먹지").build()
+        );
+        implPostRepository.save(Post.builder()
+                .authorId(2L)
+                .title("testTitle03")
+                .postBody("test postbody 03")
+                .nickName("점심뭐먹지").build()
+        );
+
+        impluserRepository.save(User.builder()
+                .email("tako@naver.com")
+                .userName("김")
+                .nickName("바나나")
+                .password("1234")
+                .phoneNo("01012345678")
+                .gender("male")
+                .build());
+
+        impluserRepository.save(User.builder()
+                .email("test@naver.com")
+                .userName("홍길동")
+                .nickName("점심뭐먹지")
+                .password("1111")
+                .phoneNo("01011112222")
+                .gender("female")
+                .build());
+    }
+
+    @Test
+    void save() {
+        Post post = new Post(1L, "title", "body", "nick");
+        Post save = implPostRepository.save(post);
+        Assertions.assertThat(save).isEqualTo(post);
+    }
+
+    @Test
+    void findById() {
+        Post post = new Post(1L, "title", "body", "nick");
+        Post save = implPostRepository.save(post);
+        Optional<Post> find = implPostRepository.findById(save.getPostId());
+        Assertions.assertThat(save).isEqualTo(find.get());
+    }
+
+    @Test
+    void findAll() {
+        List<Post> all = implPostRepository.findAll();
+        Assertions.assertThat(4).isEqualTo(all.size());
+    }
+
+    @Test
+    void update() {
+        Post savePost = implPostRepository.save(new Post(1L, "new01", "newBody01", "newNick01"));
+        Post update = new Post();
+        update.setTitle("update");
+        update.setPostBody("updateBody");
+        implPostRepository.update(savePost.getPostId(), update);
+        Optional<Post> one = implPostRepository.findById(savePost.getPostId());
+        Assertions.assertThat(one.get().getTitle()).isEqualTo(update.getTitle());
+    }
+
+    @Test
+    void delete() {
+        implPostRepository.delete(1L);
+        List<Post> all = implPostRepository.findAll();
+
+        Assertions.assertThat(all.size()).isEqualTo(3);
+    }
+}
