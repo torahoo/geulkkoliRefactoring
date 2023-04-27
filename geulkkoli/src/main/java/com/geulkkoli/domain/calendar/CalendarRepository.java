cacda7e3b5b1fbd1b76a46d2e179ad77316309a2
yoon_dahoon <111041826+dotori96@users.noreply.github.com>
2023-04-27 14:15:46 +0900

    #46 feat: 출석체크 캘린더를 위한 repository, ImplRepository을 만들었다.

---
+++ geulkkoli/src/main/java/com/geulkkoli/domain/calendar/CalendarRepository.java
@@ -0,0 +1,16 @@
+package com.geulkkoli.domain.calendar;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CalendarRepository {
+
+    Calendar save (Calendar calendar);
+
+    Optional<Calendar> findById (Long calendarId);
+    List<Calendar> findAll();
+
+
+
+
+}
