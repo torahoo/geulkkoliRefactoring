892c1fe0f7508070fba326f1a171c2558bc91009
torahoo <legokbs@gmail.com>
2023-04-27 16:48:27 +0900

    feature/#49-paging 1. homeController 및 home html수정

--- geulkkoli/src/main/java/com/geulkkoli/web/home/HomeController.java
+++ geulkkoli/src/main/java/com/geulkkoli/web/home/HomeController.java
@@ -1,18 +1,27 @@
 package com.geulkkoli.web.home;
 
+import com.geulkkoli.domain.post.service.PostService;
+import com.geulkkoli.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping("/index")
-    public String home() {
-        return "/index";
+    private final PostService postService;
+    private final UserService userService;
+
+    @GetMapping
+    public String home(Model model) {
+        model.addAttribute("list", postService.findAll());
+        return "/home";
     }
 }
 
