7da491366bc3ebec325e7e72686b810cdbf43a37
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2023-06-07 19:48:44 +0900

    #70-feat: 구독 조회 전용 서비스를 만들고 팔로워 기준으로 팔로이를 찾고, 팔로이 기준 팔로워를 찾는 걸 검증하는 테스트 코드를 작성했다.

---
+++ geulkkoli/src/main/java/com/geulkkoli/domain/follow/service/FollowFindService.java
@@ -0,0 +1,38 @@
+package com.geulkkoli.domain.follow.service;
+
+import com.geulkkoli.domain.follow.FollowEntity;
+import com.geulkkoli.domain.follow.FollowRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class FollowFindService {
+    private final FollowRepository followRepository;
+
+    public FollowFindService(FollowRepository followRepository) {
+        this.followRepository = followRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<FollowEntity> findAllFollowerByFolloweeId(Long followeeId) {
+        return followRepository.findFollowEntitiesByFollowee_UserId(followeeId);
+    }
+
+    @Transactional(readOnly = true)
+    public Integer countFollowerByFolloweeId(Long followeeId) {
+        return followRepository.countByFollowee_UserId(followeeId);
+    }
+
+    @Transactional(readOnly = true)
+    public Integer countFolloweeByFollowerId(Long followerId) {
+        return followRepository.countByFollower_UserId(followerId);
+    }
+
+    @Transactional(readOnly = true)
+    public List<FollowEntity> findAllFolloweeByFollowerId(Long followerId) {
+        return followRepository.findFollowEntitiesByFollower_UserId(followerId);
+    }
+
+}
